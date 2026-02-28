212cdb067fd8b4b73a603ef2e465bc5173b6e919
Patrícia Silva <patricia.silv.costa@gmail.com>
2026-02-28 16:46:32 -0300

    Aula dos 2 dados fim
    foi legal de fazer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,10 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal.shade700,
         appBar: AppBar(
-          title: Text('Dadoos'),
+          title: Center(
+            child: Text('2Dadoos', style: TextStyle(color: Colors.white70),
+            ),
+          ),
           backgroundColor: Colors.teal.shade900,
         ),
         body: Dadoos(),
@@ -15,9 +19,54 @@ void main() {
   );
 }
 
-class Dadoos extends StatelessWidget {
+class Dadoos extends StatefulWidget {
+  const Dadoos({super.key});
+
+  @override
+  State<Dadoos> createState() => _DadoosState();
+}
+
+class _DadoosState extends State<Dadoos> {
+  int dadoEsquerdo = 1;
+  int dadoDireito = 1;
+
+  void sorteaDados() {
+    setState(() {
+      dadoEsquerdo = Random().nextInt(6) + 1;
+      dadoDireito = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.transparent,
+              overlayColor: Colors.transparent,
+            ),
+            onPressed: () {
+              sorteaDados();
+            },
+            child: Image.asset("assets/images/dado$dadoEsquerdo.png"),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.transparent, // Colors.transparent,
+              overlayColor: Colors.transparent,
+            ),
+            onPressed: () {
+              sorteaDados();
+            },
+            child: Image.asset("assets/images/dado$dadoDireito.png"),
+          ),
+        ),
+      ],
+    );
   }
 }
